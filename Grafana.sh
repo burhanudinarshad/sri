c8f5f39df441632f291d02b2aad3a65750160e7c
Burhan U Din Arshad <45567298+burhanudinarshad@users.noreply.github.com>
2020-05-23 14:56:22 +0500

    Create Grafana.sh

---
+++ Grafana.sh
@@ -0,0 +1,15 @@
+sudo apt-get install -y apt-transport-https
+sudo apt-get install -y software-properties-common wget
+wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
+
+sudo add-apt-repository "deb https://packages.grafana.com/enterprise/deb stable main"
+
+sudo apt-get update
+sudo apt-get install grafana-enterprise
+
+sudo systemctl daemon-reload
+sudo systemctl enable grafana-server.service
+sudo systemctl start grafana-server
+sudo systemctl status grafana-server
+
+./bin/grafana-server web
