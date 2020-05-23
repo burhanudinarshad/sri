ad237449ef14647fdaa9b4ed4e2034476741bd46
Burhan U Din Arshad <45567298+burhanudinarshad@users.noreply.github.com>
2020-05-23 23:11:43 +0500

    Update Grafana.sh

--- Grafana.sh
+++ Grafana.sh
@@ -1,12 +1,16 @@
+echo "inside script"
+
 sudo apt-get install -y apt-transport-https
 sudo apt-get install -y software-properties-common wget
-wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
+sudo wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
 
+echo "get-grafana repo"
 sudo add-apt-repository "deb https://packages.grafana.com/enterprise/deb stable main"
 
 sudo apt-get update
 sudo apt-get install grafana-enterprise
 
+echo "installed-grafana repo"
 sudo systemctl daemon-reload
 sudo systemctl enable grafana-server.service
 sudo systemctl start grafana-server
