e7157af848f9054c433eab83840a5b1eadd540bb
Burhan U Din Arshad <45567298+burhanudinarshad@users.noreply.github.com>
2020-05-24 21:56:41 +0500

    Update prometheus.sh

--- prometheus.sh
+++ prometheus.sh
@@ -10,21 +10,20 @@ cd prometheus-${RELEASE}.linux-amd64/
 # Create Prometheus system group
 sudo groupadd --system prometheus
 sudo grep prometheus /etc/group
-sudo prometheus:x:999:
 
 # Create Prometheus system user
 sudo useradd -s /sbin/nologin -r -g prometheus prometheus
 sudo id prometheus
-sudo uid=999(prometheus) gid=999(prometheus) groups=999(prometheus)
 
 #Create configuration and data directories for Prometheus
 
 sudo mkdir -p /etc/prometheus/{rules,rules.d,files_sd}  /var/lib/prometheus
 
 #Copy Prometheus binary files to a directory in your $PATH
-sudo cp prometheus promtool /usr/local/bin/
+sudo cp prometheus /usr/local/bin/
+sudo cp promtool /usr/local/bin/
+
 sudo ls /usr/local/bin/
-sudo prometheus promtool
 
 #Copy consoles and console_libraries to configuration files directory
 sudo cp -r consoles/ console_libraries/ /etc/prometheus/
